5022a4d30771838b91763119f6fb70f9c2f00356
BaiJianlong <bjl125@163.com>
2025-11-25 13:35:55 +0800

    更改包名称

--- flutter/android/app/src/main/kotlin/com/baisoft/flutter_hbb/common.kt
+++ flutter/android/app/src/main/kotlin/com/baisoft/flutter_hbb/common.kt
@@ -1,4 +1,4 @@
-package com.carriez.flutter_hbb
+package com.baisoft.flutter_hbb
 
 import android.Manifest.permission.*
 import android.annotation.SuppressLint
--- flutter/android/app/src/main/kotlin/ffi.kt
+++ flutter/android/app/src/main/kotlin/ffi.kt
@@ -5,7 +5,7 @@ package ffi
 import android.content.Context
 import java.nio.ByteBuffer
 
-import com.carriez.flutter_hbb.RdClipboardManager
+import com.baisoft.flutter_hbb.RdClipboardManager
 
 object FFI {
     init {
